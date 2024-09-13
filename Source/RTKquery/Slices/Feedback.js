a634b52eb6801216a6da15990988f7703720e24b
ashishkanojia <kanojiaashish02@gmail.com>
2024-09-13 23:30:37 +0530

    adding new sclice feedback

---
+++ Source/RTKquery/Slices/Feedback.js
@@ -0,0 +1,20 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const FeedbackApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    // http://localhost:4000/api/v1/user/login
+    baseUrl: 'http://192.168.137.1:4000/api/v1/feedback',
+  }),
+  endpoints: builder => ({
+    sendFeedback: builder.mutation({
+      query: data => ({
+        url: '/sendFeedback',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {useSendFeedbackMutation} = FeedbackApi;
